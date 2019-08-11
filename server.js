c1f4a002c74fb9d2590b12e7f909d4c9cf99dd59
Evan Cutler Anway <eca7372@uncw.edu>
2019-08-11 11:38:23 -0400

    minimal working html from templating engine

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs")
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("unable to append to server.log.");
+    }
+  });
+  next();
+});
+
+app.use("/static", express.static("static"));
+
+app.get("/", (req, res) => {
+    res.redirect("/home.html");
+})
+
+app.get("/home.html", (req, res) => {
+  res.render("home.hbs");
+});
+
+app.get("/resume.html", (req, res) => {
+    res.render("resume.hbs");
+  });
+
+app.listen(port, () => {
+  console.log(`Sever is up on port ${port}`);
+});
